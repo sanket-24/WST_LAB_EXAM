7762383ea75208177af1bd54db832d9756578755
sanket-24 <patilsr2024@gmail.com>
2021-05-30 18:12:26 +0530

    all done with backend

--- server/router/create.js
+++ server/router/create.js
@@ -11,7 +11,7 @@ Router.get("/create", async(req, res) => {
 
         const existingProduct = await Product.findOne({product_name})
 
-        if(newProduct){
+        if(existingProduct){
             return res.send("product already registered !!");
         }
         const newProduct = new Product({
@@ -23,3 +23,53 @@ Router.get("/create", async(req, res) => {
         res.status(404).json({message : error.message});
     }
 });
+Router.get("/update", async(req, res) => {
+    let product_name = req.query.name;
+    let product_category = req.query.category;
+    let product_quantity = req.query.quantity;
+    let product_price = req.query.price;
+    console.log(name);
+    try {
+        
+
+        const existingProduct = await Product.findOne({product_name})
+
+        if(existingProduct){
+
+            existingProduct.product_category = product_category;
+            existingProduct.product_price = product_price;
+            existingProduct.product_quantity = product_quantity;
+            existingProduct.save();
+            return res.send("product updated !!");
+        }
+    }
+        catch (error) {
+            res.status(404).json({message : error.message});
+        }
+       
+});
+
+Router.get("/delete", async(req, res) => {
+    let product_name = req.query.name;
+    let product_category = req.query.category;
+    let product_quantity = req.query.quantity;
+    let product_price = req.query.price;
+    console.log(name);
+    try {
+        
+
+        const existingProduct = await Product.findOne({product_name})
+
+        if(existingProduct){
+
+            
+            existingProduct.delete();
+            return res.send("product deleted !!");
+        }
+    }
+        catch (error) {
+            res.status(404).json({message : error.message});
+        }
+       
+});
+
